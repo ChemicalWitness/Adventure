84449f496ae57f7f4900a4e0035adfce52ea5c4f
Igor <trololol-lolo@mail.ru>
2023-09-03 19:13:09 +1000

    Небольшие правки в скрипте

--- source/js/modules/audio.js
+++ source/js/modules/audio.js
@@ -1,12 +1,12 @@
-const slides = document.querySelectorAll('.hero__slider-slide');
+const slides = document.querySelectorAll('.hero__slide');
 const pagination = document.querySelector('.hero__pagination-wrapper');
 const audio = document.querySelector('.hero__slide-audio');
 
-export const initAudio = () => {
+export const initAudio = () =>
   pagination.addEventListener('click', () => {
     slides.forEach((slide) => {
 
-      if (slide.querySelector('.hero-card__inner-container-audio') &&
+      if (slide.querySelector('.hero__slide-audio') &&
         slide.classList.contains('swiper-slide-active')) {
         audio.src = 'https://music.yandex.ru/iframe/#track/112912322/25474374';
       } else {
@@ -14,4 +14,4 @@ export const initAudio = () => {
       }
     });
   });
-};
+
