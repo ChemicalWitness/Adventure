55dc85ae4d7c024503fba73ba16bce77e6c43076
Igor <trololol-lolo@mail.ru>
2023-09-06 20:31:40 +1000

    Оптимизация

--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -9,6 +9,7 @@ export const mapInit = () => {
     iframe.setAttribute('src', 'https://yandex.ru/map-widget/v1/?ll=37.630782%2C55.774836&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NjczMzc2MxI80KDQvtGB0YHQuNGPLCDQnNC-0YHQutCy0LAsINC_0YDQvtGB0L_QtdC60YIg0JzQuNGA0LAsIDE00YExIgoN2ocWQhVvGV9C&z=17.13');
     iframe.setAttribute('allowfullscreen', '');
     iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('loading', 'lazy');
 
     mapIframe.classList.add('is-shown');
     mapIframe.appendChild(iframe);
